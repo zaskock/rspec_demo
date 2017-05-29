05e3254ce79337feeaf7794193c77562a6bfc355
Valera <bomzadua@yomail.info>
2017-05-30 02:02:56 +0300

    bundle install executed

---
+++ vendor/ruby/2.0.0/bin/autospec.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"d:\Ruby200\bin\ruby.exe" "D:/Ruby projects/rspec_demo/vendor/ruby/2.0.0/bin/autospec" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"d:\Ruby200\bin\ruby.exe" "%~dpn0" %*
