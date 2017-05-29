70dd1913373805661e0fdba80dc964571266ab6b
Valera <bomzadua@yomail.info>
2017-05-30 01:12:32 +0300

    hero entity added

--- hero.rb
+++ hero.rb
@@ -1,2 +1,32 @@
 class Hero
+
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
+	end
+	
+	def name
+	@name
+	end
+
+	def power_up
+	@health+=10
+	end
+	
+	def power_down
+	@health-=10
+	end
+
+	def power_down
+	@health-=10
+	end
+
+	def info
+	"#{@name} has #{@health} health points"
+	end
+
 end
+
+hero = Hero.new 'mike'
+puts hero.name
+puts hero.info
