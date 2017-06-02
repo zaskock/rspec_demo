f6e8c5cdadc83eb77ed59ee73f1b42ed51df9cfd
Valera <bomzadua@yomail.info>
2017-06-03 01:40:50 +0300

    power_down and hero_info added

--- hero_spec.rb
+++ hero_spec.rb
@@ -14,4 +14,16 @@ describe Hero do
 		expect(hero.power_up).to eq 110
 	end
 
+	it "can power up" do
+		hero= Hero.new 'mike'
+
+		expect(hero.power_down).to eq 90
+	end
+
+	it "displays full hero info" do
+		hero= Hero.new 'mike'
+
+		expect(hero.info).to eq "Mike has 100 health points"
+	end
+
 end
