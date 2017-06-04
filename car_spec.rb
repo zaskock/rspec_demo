64fb9f6a9861a91e035379d78804a9e642f2cbb6
Valera <bomzadua@yomail.info>
2017-06-04 03:06:06 +0300

    fixes #1

--- car_spec.rb
+++ car_spec.rb
@@ -4,16 +4,16 @@ describe Car do
 
 #arrange
 	before do 
-		car= Car.new 
+		@car= Car.new 
 	end
 
 	it "returns range" do
 
 #act
-		car.add_fuel 10
+		@car.add_fuel 10
 
 #assert
-		expect(car.range).to eq 200
+		expect(@car.range).to eq 200
 	end
 
 end
